9b14902ded64bae7c0248d5b9102d74422f32668
Kevin Xu <kevinpxu21@gmail.com>
2022-11-04 11:50:06 -0500

    added routes to product and tags

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -35,16 +35,50 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(201).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    await Category.update(
+      { category_name: req.body.category_name },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(204);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: { id: req.params.id },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: "No category with that ID found" });
+      return;
+    }
+    res.status(200);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
